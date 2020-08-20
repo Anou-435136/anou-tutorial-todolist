31465ac8da6dd417c293159c4dd8d1f9707655b5
Anou <aanou1129@icloud.com>
2020-08-20 15:50:03 +0900

    Complete-commit

--- todolist/main.js
+++ todolist/main.js
@@ -1,77 +1,100 @@
-import Vue from "vue";
-
-var STORAGE_KEY = "todos-vuejs-demo"
+var STORAGE_KEY = "todos-vuejs-demo";
 var todoStorage = {
-  fetch: function () {
-    var todos = JSON.parse(
-      localStorage.getItem(STORAGE_KEY) || "[]"
-    )
-    todos.forEach(function (todo, index) {
-      todo.id = index
-    })
-    todoStorage.uid = todos.length
-    return todos
+  fetch: function() {
+    var todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
+    todos.forEach(function(todo, index) {
+      todo.id = index;
+    });
+    todoStorage.uid = todos.length;
+    return todos;
   },
-  save: function (todos) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
-  }
-}
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+  },
+};
 
+// ★STEP1
 new Vue({
   el: "#app",
 
   data: {
+    // ★STEP5 localStorage から 取得した ToDo のリスト
     todos: [],
+    // ★STEP11 抽出しているToDoの状態
+    current: -1,
+    // ★STEP11＆STEP13 各状態のラベル
     options: [
       { value: -1, label: "すべて" },
       { value: 0, label: "作業中" },
-      { value: 1, label: "完了" }
+      { value: 1, label: "完了" },
     ],
-    current: -1
   },
-  
   computed: {
-    computedTodos: function () {
-      return this.todos.filter(function (el) {
-        return this.current < 0 ? true : this.current === el.state
-      }, this)
+    // ★STEP12
+    computedTodos: function() {
+      return this.todos.filter(function(el) {
+        return this.current < 0 ? true : this.current === el.state;
+      }, this);
     },
+
+    // ★STEP13 作業中・完了のラベルを表示する
     labels() {
-      return this.options.options.reduce(function (a, b) {
-        return Object.assign(a, { [b.value]: b.label })
-      }, {})
-    }
+      return this.options.reduce(function(a, b) {
+        return Object.assign(a, { [b.value]: b.label });
+      }, {});
+      // キーから見つけやすいように、次のように加工したデータを作成
+      // {0: '作業中', 1: '完了', -1: 'すべて'}
+    },
   },
+
+  // ★STEP8
+  watch: {
+    // オプションを使う場合はオブジェクト形式にする
+    todos: {
+      // 引数はウォッチしているプロパティの変更後の値
+      handler: function(todos) {
+        todoStorage.save(todos);
+      },
+      // deep オプションでネストしているデータも監視できる
+      deep: true,
+    },
+  },
+
+  // ★STEP9
   created() {
-    this.todos = todoStorage.fetch()
+    // インスタンス作成時に自動的に fetch() する
+    this.todos = todoStorage.fetch();
   },
   methods: {
-    doAdd: function (event, value) {
-      let comment = this.$refs.comment
+    // ★STEP7 ToDo 追加の処理
+    doAdd: function(event, value) {
+      // ref で名前を付けておいた要素を参照
+      var comment = this.$refs.comment;
+      // 入力がなければ何もしないで return
       if (!comment.value.length) {
-        return
+        return;
       }
+      // { 新しいID, コメント, 作業状態 }
+      // というオブジェクトを現在の todos リストへ push
+      // 作業状態「state」はデフォルト「作業中=0」で作成
       this.todos.push({
         id: todoStorage.uid++,
         comment: comment.value,
-        state: 0
-      })
-      comment.value = ""
+        state: 0,
+      });
+      // フォーム要素を空にする
+      comment.value = "";
     },
-    doChangeState: function (item) {
-      item.state = item.state ? 0 : 1
+
+    // ★STEP10 状態変更の処理
+    doChangeState: function(item) {
+      item.state = !item.state ? 1 : 0;
     },
-    doRemove: function (item) {
-      let index = this.todos.indexOf(item)
-      this.todos.splice(index, 1)
-    }
+
+    // ★STEP10 削除の処理
+    doRemove: function(item) {
+      var index = this.todos.indexOf(item);
+      this.todos.splice(index, 1);
+    },
   },
-  watch: {
-    todos: {
-      handler: function (todos) {
-        todoStorage.save(todos)
-      },
-      deep: true
-    }
-  }
-})
+});
